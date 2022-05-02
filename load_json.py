96ada2f4778df44f5b37972d37f9e92c42f819a8
Natalia Golub <golub.togo@gmail.com>
2022-05-02 15:10:40 +0200

    counter

--- load_json.py
+++ load_json.py
@@ -32,3 +32,51 @@ for item in data_list:
 print(table_clients)
 
 
+c = collections.Counter()
+for table_clients in table_clients:
+    c[table_clients] += 1
+print(c)
+
+print(len(c.keys()))
+print(c[60459])
+
+
+table_all_clients = []
+for item in data_list:
+    client_id = item['client_id']
+    category = item['category']
+    table_all_clients.append(client_id)
+
+print(table_all_clients)
+
+
+c = collections.Counter()
+for table_all_clients in table_all_clients:
+    c[table_all_clients] += 1
+
+
+print(c)
+print(len(c.keys()))
+print(c[60459])
+
+
+page_clients = []
+for item in data_list:
+    client_id = item['client_id']
+    category = item['category']
+    if category == 'page':
+        page_clients.append(client_id)
+print(c)
+print(c[62602])
+
+
+report_clients = {}
+for item in data_list:
+    client_id = item['client_id']
+    category = item['category']
+    if category == 'report':
+        report_clients.setdefault(client_id)
+print(len(report_clients))
+print(report_clients)
+
+
